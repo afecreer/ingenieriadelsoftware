16b4906d85db5ff7874d9a315376132bddba2ed1
afecreer <your-email@example.com>
2024-08-27 17:44:07 -0500

    Clase del 27082024 Ingennieria del software

---
+++ frases.py
@@ -0,0 +1,9 @@
+frase1 = "Disfruto crear software innovador"
+frase2 = "Una estudiante deja de hacer tareas y sale en busca de entretenimiento"
+frase3= "Aldair se fue a comer y olvidó hacer su código"
+frase4 = "Ellos tenían una cubeta llena de agua, mientras comían unos dorilocos"
+frase5 = "Anoche los ratones se comieron el queso"
+frase6 = "El flujo de energia vital es esencial para mantner el equilibrio y la armonia de nuestras vidas"
+frase7 = "La música trae color a mi mundo"
+frase_completa = frase1 +" " +frase2 + " " +frase3 + " " +frase4 + " " +frase5 + " " +frase6 + " " +frase7  
+print(frase_completa)
